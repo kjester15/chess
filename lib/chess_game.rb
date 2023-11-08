2986a39e7bcb167dc9f034746323590a9303e648
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-11-07 17:51:30 -0800

    Create Game class framework

--- lib/chess_game.rb
+++ lib/chess_game.rb
@@ -2,6 +2,11 @@ require_relative 'chess_board'
 require_relative 'chess_piece'
 require_relative 'chess_player'
 
+class Game
+  def initialize
+  end
+end
+
 piece = Piece.new
 board = Board.new
 puts piece.pieces[:king_w]
