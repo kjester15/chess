f20eadf2de78a08485daefa7481eb1dc55b1f312
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-11-10 17:20:04 -0800

    Move gameplay into #run_game

--- lib/chess_game.rb
+++ lib/chess_game.rb
@@ -24,16 +24,39 @@ class Game
     puts "White goes first. #{@player1.name}, your turn!"
   end
 
-  # def player_turn
-  # end
+  def player_turn
+    # Retrieve player move (CREATE NEW METHOD)
+    #   enter a move to make - piece letter followed by move location
+    # Validate move (CREATE NEW METHOD)
+    #   take the entered text and separate the first letter and move location
+    #     validate that the first letter is correct (K/Q/R/B/N/pawn is '')
+    #     validate that the move location is a valid spot on the board
+    # Determine potential spaces piece could move from (CREATE NEW METHOD)
+    #   run a possible move method on the move location based on the noted letter and add to array
+    # Check for piece (CREATE NEW METHOD)
+    #   check if the noted piece is in any of the move locations in the previously created array
+    # Move (and replace) piece (CREATE NEW METHOD)
+    #   if the piece is found, move the piece to the new location
+    #     if the piece is not found, reprompt the entire process
+    #       if multiples pieces are found, reprompt user input with file, rank, or rank and file,
+    #       until only one option remains
+    #   if a piece is captured, replace it with the players piece
+    #     add an x to the player's entered move before the location (Kxg5)
+    # Add move to log (CREATE NEW METHOD)
+    #   the player's move text to an array of moves that have been made
+  end
+
+  def run_game
+    board = Board.new
+    board.populate_board
+    board.print_board
+    # piece = Piece.new('king', 'black', [3, 3])
+    # piece.king_moves(piece.coordinate)
+  end
 
   # def save_game
   # end
 end
 
-board = Board.new
-board.populate_board
-board.print_board
-piece = Piece.new('king', 'black', [3, 3])
-piece.king_moves(piece.coordinate)
-print piece.possible_moves
+game = Game.new
+game.run_game
